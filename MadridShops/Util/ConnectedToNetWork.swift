c4c115c8730aaedbfea24ac0d12ab6dbe56c0bde
yisus <yisus@yisuss-Mac.local>
2017-09-30 04:54:20 -0700

    Create ConnectedToNetWork

---
+++ MadridShops/Util/ConnectedToNetWork.swift
@@ -0,0 +1,39 @@
+//
+//  ConnectedToNetWork.swift
+//  MadridShops
+//
+//  Created by yisus on 29/09/2017.
+//  Copyright © 2017 yisus. All rights reserved.
+//
+
+// https://stackoverflow.com/questions/25623272/how-to-use-scnetworkreachability-in-swift
+
+import SystemConfiguration
+
+class Network {
+    
+    func isConnected () -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return (isReachable && !needsConnection)
+    }
+    
+}
